a70d9dbd1fe15728fde38c6917e32dcaa95d1f25
ysdasss <yensgruijter@gmail.com>
2025-12-28 16:25:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 def main():
+    # Start Flask OAuth server
     threading.Thread(target=run_web, daemon=True).start()
+
+    # Start Discord bot
     run_bot(BOT_TOKEN)
 
 if __name__ == "__main__":
